10659ef31907ca738f9da9ed645ec3aa5bdee6dc
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-15 14:43:45 +0700

    init

--- src/router/GetInfo.js
+++ src/router/GetInfo.js
@@ -9,9 +9,9 @@ GetInfo
         try {
             const allArchive = await odoo.searchRead("customer.archive", [], ['id', 'name', 'homeLocation', 'project', 'phone', 'user_id'])
             const allBill = await odoo.searchRead("customer.bill", [], ['type', 'id', 'total', 'status', 'user_id'])
-            console.log(allArchive, allBill)
+            // console.log(allArchive, allBill)
 
-            return res.status(200).json(myTask)
+            return res.status(200).json(allArchive)
         }
         catch {
             return res.status(404).json()
