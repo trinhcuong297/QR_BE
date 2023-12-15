1aafe0c292c19f672a8752f88a723bc694343cad
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-15 14:35:33 +0700

    init

--- src/router/Login.js
+++ src/router/Login.js
@@ -8,7 +8,7 @@ Login
     .post('/', async (req, res) => {
         try {
             const User = await odoo.searchRead("customer.account", ["&", ['email', '=', req.body.email], ['password', '=', req.body.password]])
-            return res.status(200).json(User)
+            return res.status(200).json("User")
         }
         catch {
             return res.status(404).json("")
