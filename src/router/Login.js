6be78c8ccbd961451eb4941a3419a0e60b5d8420
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-15 15:50:05 +0700

    init

--- src/router/Login.js
+++ src/router/Login.js
@@ -7,6 +7,7 @@ const Login = express.Router();
 Login
     .post('/', async (req, res) => {
         try {
+            console.log(req.body)
             const User = await odoo.searchRead("customer.account", ["&", ['email', '=', req.body.email], ['password', '=', req.body.password]])
             return res.status(200).json(User)
         }
