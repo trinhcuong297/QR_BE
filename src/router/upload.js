771ccbebbae098debc99f16e52798ec19757b911
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-18 10:02:48 +0700

    changeAPI

--- src/router/upload.js
+++ src/router/upload.js
@@ -77,6 +77,7 @@ Upload
                 return res.status(200).json("OK")
             }
             catch (err) {
+                console.log(err)
                 res.status(500).json(err)
             }
         })
